190873ec357dd7faa031218e028f6adeebce4c96
Kelly online <kellyonline2016@gmail.com>
2020-02-05 18:01:35 +0100

    modify pages

--- pages.js
+++ pages.js
@@ -3,4 +3,29 @@ let url = require("url")
 let fileSystem = require("fs")
 
 
-http.createServer().listen(8081)
+http.createServer(function (request, response) {
+    let newUrl = url.parse(request.url, true)
+    let fileName = "." +  newUrl.pathname
+
+    if (fileName == './') {
+        fileName = './index.html'
+    }
+
+     
+    console.log(fileName)
+
+
+    // read the files
+
+    fileSystem.readFile(fileName, function (error, content) {
+        
+        if (error) {
+            response.writeHead(404)
+            return response.end("Page not Found")
+        }
+
+        response.writeHead(200)
+        response.write(content)
+        return response.end()
+    })
+}).listen(8081)
