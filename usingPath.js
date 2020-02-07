655e5740bc23d10e7473ee924abd2ad53a8bbe60
Kelly online <kellyonline2016@gmail.com>
2020-02-07 21:59:57 +0100

    set gateway

--- usingPath.js
+++ usingPath.js
@@ -2,3 +2,7 @@ const path = require('path')
 const fileSystem = require('fs')
 const express = require('express')
 const url = require('url')
+
+const server = express()
+
+server.listen(3500, () => {Console.log("Server is listening on port 3500")})
