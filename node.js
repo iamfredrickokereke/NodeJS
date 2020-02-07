9c5b254904a9926a658ed63eb9526f6d7b1ca878
Kelly online <kellyonline2016@gmail.com>
2020-02-07 20:43:36 +0100

    Understanding Synchronous and Asynchronous

---
+++ node.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const fs = require("fs")
+
+const server = http.createServer( (request, response) => {
+    //response.statusCode = 200
+
+    //console.log(request.url)
+
+
+    //Blocking and Non Blocking Code
+
+    const secondPage = fs.readFileSync("./second.html")
+    const thirdPage = fs.readFileSync("./third.html")
+    const firstPage = fs.readFileSync("./index.html")
+
+
+    if (request.url === '/'){
+        return response.end(firstPage)
+    }else if (request.url === '/second'){
+        return response.end(secondPage)
+    }else if (request.url === '/third'){
+        return response.end(thirdPage)
+    }else{
+        response.writeHead(404)
+        return response.end("PAGE NOT FOUND")
+    }
+    
+})
+
+server.listen(3000)
