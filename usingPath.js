d2d1fd349142c164d221afba8d38976919ff84f4
Kelly online <kellyonline2016@gmail.com>
2020-02-07 23:58:45 +0100

    using conditions

--- usingPath.js
+++ usingPath.js
@@ -5,9 +5,20 @@ const url = require('url')
 
 const server = express()
 
-server.get("./index.html", (request, response) => {
-    response.sendFile("index.html", () =>{ alert("file not found")})
+server.get("/index", (request, response) => {
+   
+
+    if (request.url === '/index') {
+
+        response.sendFile(path.resolve(__dirname, 'third.html'))
+ 
+    }else{
+      return  response.end("fake file")
+    }
+
 })
+    
+    
 
 //server.post(a,b)
 
